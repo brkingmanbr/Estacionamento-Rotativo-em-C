87ce4f111c37eaac120cc39fb5dce30496273816
Júlio Magalhães Barbosa <julio030496@gmail.com>
2015-11-24 16:14:50 -0300

    Mais Conflitos resolvidos mas continua com o erro: Segmentation fault (core dumped)

--- EstacionamentoRotativo.c
+++ EstacionamentoRotativo.c
@@ -25,6 +25,7 @@ int pegaHora(char hora[]);
 int proximaPosicaoLivreNoRegistro();
 int placaNoRelatorio(char placa[]);
 void fecharEstacionamento();
+
 	
 	char relatorioPlacas[100][8];
 	int relatorioTempo[100];
@@ -39,7 +40,7 @@ void fecharEstacionamento();
 int main(int argc, char **argv)
 {
 	int resposta;
-	do{
+	
 	resposta = 0;
 	char placa[8];
     char hora[5];    	
@@ -49,40 +50,43 @@ int main(int argc, char **argv)
 	printf("\nDigite 3 para: Fechar o Estacionamento");
 	scanf("%i", resposta);
 	
+	while(resposta != 3){
+		if(resposta == 1){
+			printf("Digite sua placa por favor\n");
+			scanf("%s", placa);
+			printf("Digite que horas são no formato 24 Hrs\n");
+			scanf("%s", hora);
+			
+			if(estacione(placa, pegaHora(hora)) < 20){
+				
+			printf("\nEstacione na vaga: %d", estacione(placa, pegaHora(hora)));
+			 }else{
+			  printf("\nNão temos vagas volte mais tarde");
+			}		
+			
+			
+		}
+		if(resposta == 2){
+			printf("Digite sua placa por favor\n");
+			scanf(" %s", placa);
+			printf("Digite que horas são no formato 24 Hrs");
+			scanf(" %s", hora);
+			printf("Pelo tempo que voce ficou deverá pagar: %f", SairDoEstacionamento(placa, hora));
+			
+		}
+		system("cls");
+	}
+	//caso a resposta seja = 3;
+	fecharEstacionamento();	
 	
-	if(resposta == 1){
-        printf("Digite sua placa por favor\n");
-		scanf("%s", placa);
-		printf("Digite que horas são no formato 24 Hrs\n");
-		scanf("%s", hora);
-		
-		if(estacione(placa, pegaHora(hora)) < 20){
-            
-  		printf("\nEstacione na vaga: %d", estacione(placa, pegaHora(hora)));
-         }else{
-          printf("\nNão temos vagas volte mais tarde");
-        }		
-		
-		
-	}
-	if(resposta == 2){
-		printf("Digite sua placa por favor\n");
-		scanf(" %s", placa);
-		printf("Digite que horas são no formato 24 Hrs");
-		scanf(" %s", hora);
-		printf("Pelo tempo que voce ficou deverá pagar: %f", SairDoEstacionamento(placa, hora));
-		
-	}
-	system("cls");
-	}while(resposta != 3);
-
+	return 0;
 }
 
 int estacione(char placa[], int hora){
 	int cont = 0;
 	int vagaDisponivel, x;
 	int achou = 0;
-	do{
+	do{	
 		if(vagas[cont] = 0){
 			achou = 1;
 			vagaDisponivel = cont;
@@ -93,8 +97,8 @@ int estacione(char placa[], int hora){
 			horaEntrada[cont] = hora;
 		}
 		cont+= 1;			
-	}while(achou == 0 && cont == 19);
-if(cont == 19 && achou == 0) vagaDisponivel = 20;	
+	}while(achou == 0 && cont < 20);
+if(cont == 20 && achou == 0) vagaDisponivel = 20;	
 	
  return vagaDisponivel;	
 }
@@ -103,21 +107,23 @@ float SairDoEstacionamento(char placa[], char horaDaSaida[]){
 	
 	int tempoEstacionado;
 	float valorAPagar;
+	int HorasEstacionado;
 	int local = vaga(placa);
 	vagas[local] = 0;
-	for(int cont = 0; cont < 8; cont ++){
+	int cont;
+	for(cont = 0; cont < 8; cont ++){
 		carros[local][cont] = 0;
 	}
 	tempoEstacionado = horaEntrada[local] - pegaHora(horaDaSaida);
 	if(tempoEstacionado < 60){
 		valorAPagar = PrecoDoMinuto * tempoEstacionado;
 	}else{
-		int HorasEstacionado = tempoEstacionado/60;
+		HorasEstacionado = tempoEstacionado/60;
 		valorAPagar = (PrecoDoMinuto*60) * (HorasEstacionado + 1);
 	}
-	
+	int x;
 	int posicao = placaNoRelatorio(placa);
-	for(int x = 0; x < 8; x++) relatorioPlacas[posicao][x] = placa[x];
+	for(x = 0; x < 8; x++) relatorioPlacas[posicao][x] = placa[x];
 	relatorioTempo[posicao] = tempoEstacionado;
 	relatorioTotal += valorAPagar;
 
@@ -128,9 +134,9 @@ int vaga(char placa[]){
 	int vagaOndeEsta;
 	int i = 0;
 	int x = 0;
-	
+	int k;
 	for(i = 0; i < 20; i++){
-		for(int k = 0; k < 8; k++){
+		for(k = 0; k < 8; k++){
 			if(placa[x] == carros[i][x] && placa[x] != '\0' && carros[i][x] != '\0') x++;
 		}
 		if(placa[x] == '\0' && carros[i][x] == '\0'){
@@ -147,9 +153,9 @@ int placaNoRelatorio(char placa[]){
 	int OndeEstaNoRelatorio;
 	int i = 0;
 	int x = 0;
-	
+	int k = 0;
 	for(i = 0; i < 100; i++){
-		for(int k = 0; k < 8; k++){
+		for( k = 0; k < 8; k++){
 			if(placa[x] == relatorioPlacas[i][x] && placa[x] != '\0' && relatorioPlacas[i][x] != '\0') x++;
 		}
 		if(placa[x] == '\0' && relatorioPlacas[i][x] == '\0'){
@@ -159,28 +165,27 @@ int placaNoRelatorio(char placa[]){
 		}
 			
 	}
+
 	if(i == 100 && OndeEstaNoRelatorio == 0) OndeEstaNoRelatorio = proximaPosicaoLivreNoRegistro();
- return OndeEstaNoRelatorio;	
+	return OndeEstaNoRelatorio;	
 }
 
-int pegaHora(char hora[]){
+int pegaHora(char hora[])
+{
     int H;
     int M;
 	
-if(hora[3] == '\0'){
+if(hora[3] == '\0')
+{
     H = (hora[0]*10)+hora[1];
 		M = (hora[2]*10)+hora[3];
-	}else{
+	}
+	else
+	{
 		H = hora[0];
 		M = (hora[1]*10)+hora[2];
-	}
+			}
 		return (H*60)+M;
-	}
-
-	void fecharEstacionamento(){
-		system("cls");
-	
-	
 }
 
 int proximaPosicaoLivreNoRegistro(){
@@ -188,3 +193,7 @@ int proximaPosicaoLivreNoRegistro(){
 	for(x = 0; relatorioTempo != 0; x++);
 	return x;
 }
+
+void fecharEstacionamento(){
+		system("cls");
+		}
